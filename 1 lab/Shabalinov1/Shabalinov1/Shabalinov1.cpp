0d3d6286c6283bd4d58ca1be1743192039896171
beGG1 <70636894+beGG1@users.noreply.github.com>
2020-09-24 20:30:00 +0300

    Улучшение кода
    Почищен код, настроен русский язык, прописаны комментарии

--- 1 lab/Shabalinov1/Shabalinov1/Shabalinov1.cpp
+++ 1 lab/Shabalinov1/Shabalinov1/Shabalinov1.cpp
@@ -4,53 +4,82 @@
 
 #include <iostream>
 #include <fstream>
+#include <Windows.h> // Нужно для распознания кирилицы 
+
 
 using namespace std;
 
+    // Структура трубы
 struct Pipe
 {
-    int id;
+    size_t id;
     int lenght;
     int diametr;
     bool repear;
 };
 
+    // Создание трубы
 Pipe createPipe() {
     Pipe P;
-    cout << "Input id ";
-    cin >> P.id;
-    cout << "Input lenght ";
-    cin >> P.lenght;
-    cout << "Input diametr ";
-    cin >> P.diametr;
-    P.repear = false;
+
+    // Ввод и проверка положительности длинны
+    do {
+        cout << "Введите длину: ";
+        cin >> P.lenght;
+        if (P.lenght <= 0) {
+            cout << "Ошибка. Ведите положительное число" << endl;
+        }
+    } while (P.lenght <= 0);
+    
+    // Ввод и проверка положительности диаметра
+    do {
+        cout << "Введите диаметр: ";
+        cin >> P.diametr;
+        if (P.diametr <= 0) {
+            cout << "Ошибка. Ведите положительное число" << endl;
+        }
+    } while (P.diametr <= 0);
+
+        P.repear = false;
     return P;
 }
 
+    // Изменение состояния ремонта
 void editPipe(Pipe P) {
-    if (P.repear == false) {
-        P.repear = true;
-    }
-    else {
+    if (P.repear) {
         P.repear = false;
     }
-}
+    else {
+        P.repear = true;
+    }
+ }
 
+
+    // Вывод данных о трубе в консоль
 void printPipe(Pipe P)
 {
-    cout << "id " << P.id << "  " << "Lenght " << P.lenght << "  " << "Diametr " << P.diametr << "  " << "Repear " << P.repear << endl;
+    cout << "id: " << P.id << endl << "Длина: " << P.lenght << endl << "Диаметр: " << P.diametr << endl;
+    if (P.repear) {
+        cout << "Состояние ремонта: В ремонте" << endl;
+    }
+    else {
+        cout << "Состояние ремонта: Не в Ремонте" << endl << endl;
+    }
 
 }
 
+    //Сохранение в файл
 void savePipe(const Pipe& P) {
     ofstream fout;
-    fout.open("data.txt", ios::out);
+    fout.open("Pipe.txt", ios::out);
     if (fout.is_open())
     {
         fout << P.id << endl << P.lenght << endl << P.diametr << endl << P.repear;
         fout.close();
     }
 }
+
+    //Загрузка из файла
 Pipe loadPipe() {
     ifstream fin;
     fin.open("data.txt", ios::in);
@@ -65,13 +94,14 @@ Pipe loadPipe() {
 
 int main()
 {
+    // Распознание кириллицы в консоли  (https://ru.stackoverflow.com/questions/117144/Русские-символы-при-вводе-и-выводе-на-c)
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251); // Вывод на консоль в кодировке 1251. Нужно только будет изменить шрифт консоли на Lucida Console или Consolas
+
     Pipe P1 = createPipe();
     printPipe(P1);
     editPipe(P1);
     printPipe(P1);
-    editPipe(P1);
-    printPipe(P1);
-    savePipe(P1);
     return 0;
 
 }
